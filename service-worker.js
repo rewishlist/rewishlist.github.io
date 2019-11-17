ef6b9678f882041ce7f8b87f3c99acabe3b87952
Gary Anikin <garyanikin@gmail.com>
2019-11-17 02:58:16 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d17d57a58e048595c6a59d487ab0186.js"
+  "/precache-manifest.8cc012da2c064f34da6d6e0ebab4c433.js"
 );
 
 self.addEventListener('message', (event) => {
